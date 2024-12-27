4d59dd6fb98b7d782cb7fe9d6f5e51faae21ea4f
Luca Pircher <luca.pircher@hotmail.com>
2024-12-27 09:38:31 +0000

    Fix typo

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,7 @@
 export default function Checkout() {
   return (
     <p className="text-lg p-8">
-      A user would be redirect to an affiliate network and then to a shop from
+      A user would be redirected to an affiliate network and then to a shop from
       here.
     </p>
   );
